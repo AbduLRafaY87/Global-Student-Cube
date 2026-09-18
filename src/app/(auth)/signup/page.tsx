a1e2abd87c3d22cebd785900d7579539e140d309
Abdul Rafay <abdulrafay.chatriwala@gmail.com>
2026-09-19 00:19:45 +0500

    Email Verification Fixed

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -91,6 +91,7 @@ export default function SignupPage() {
         password,
         options: {
           data: { role },
+          emailRedirectTo: `${window.location.origin}/auth/callback`,
         },
       });
 
